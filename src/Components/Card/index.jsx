6817e725451b5eb3d016df62d242a4ad55045beb
LuciaNishimiya <lucianishimiya@gmail.com>
2023-10-31 21:49:03 -0300

    Added cart component

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,21 @@
+
+
+function Card({ price, title, category, image }) {
+    return (
+        <article className=' cursor-pointer w-56 h-60 rounded-lg p-4'>
+
+            <figure className='relative mb-2 w-full h-4/5'>
+                <figcaption className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{category}</figcaption>
+                <img className='w-full h-full object-cover rounded-lg' src={image} alt={title} />
+                <button className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' > + </button>
+            </figure><p className='flex justify-between'>
+                <span className='text-sm font-light'>{title}</span>
+                <span className='text-lg font-medium'>{price}</span></p>
+
+
+
+        </article>
+    )
+}
+
+export default Card
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,14 @@
-
+import Card from "../../Components/Card"
+import { products } from "../../Services/products"
 function Home() {
-
+  const { data, isLoading, error } = products()
   return (
-    <div>
+    <div className='grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-screen-lg px-2'>
 
-
+      {
+        data.map(item =>
+          (<Card key={item?.id} price={item?.price} title={item?.title} category={item?.category} image={item?.image}></Card>))
+      }
     </div>
   )
 }
