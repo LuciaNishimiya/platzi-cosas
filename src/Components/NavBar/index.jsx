69566dcd8c6001a7573d0c511455cc6b6328406c
LuciaNishimiya <lucianishimiya@gmail.com>
2023-12-01 19:35:04 -0300

    Change icons to heroicons svg

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const { data } = categories()
     const activeStyle = 'underline underline-offset-4'
     return (
-        <nav className='flex justify-between items-center bg-white dark:bg-zinc-900 fixed z-10 w-full py-5 px-8 text-sm font-light top-0'>
+        <nav className='flex justify-between items-center bg-gray-50 dark:bg-zinc-900 fixed z-10 w-full py-5 px-8 text-sm font-light top-0 '>
             <ul className='flex items-center gap-3'>
                 <li className='font-semibold text-lg'>
                     <NavLink to='/'>
@@ -68,8 +68,11 @@ const Navbar = () => {
                         Sign In
                     </NavLink>
                 </li>
-                <li>
-                    🛒 {cart.length}
+                <li className='flex justify-center'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 m-0.5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                    </svg>
+                    <span>{cart.length}</span>
                 </li>
             </ul>
 
