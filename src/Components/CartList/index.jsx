24fa20ed3aa8862be1242d8bf7f717ce3f2e93a6
LuciaNishimiya <lucianishimiya@gmail.com>
2023-12-02 02:27:30 -0300

    Rewrite Card LIst styles

--- src/Components/CartList/index.jsx
+++ src/Components/CartList/index.jsx
@@ -4,32 +4,56 @@ import { useContext } from "react";
 function CartList({ price, title, category, image, id, quantity }) {
     const { addToCart, removeFromCart } = useContext(CartContext);
     return (
-        <article className=' flex w-600 h-60  p-4'>
-            <figure className='relative mb-2 w-full h-4/5'>
-
-                <img className=' w-56 h-48 object-cover rounded-lg' src={image} alt={title} />
-                <button className='absolute top-0 right-0 flex justify-center items-center bg-white w-16 h-6 rounded-full m-2 p-1'
-                    onClick={() => addToCart({ price, title, category, image, id })} >
-                    ➕
-                </button>
-                <button className='absolute top-0 right-20 flex justify-center items-center bg-white w-16 h-6 rounded-full m-2 p-1'
-                >
-                    ➖
-                </button>
-                <button className='absolute top-0 right-40 flex justify-center items-center bg-white w-16 h-6 rounded-full m-2 p-1'
-                    onClick={() => removeFromCart({ id })} >
-                    🗑️
-                </button>
-                <p className='absolute  top-9 left-56 m-2 p-1'>Quantity: {quantity}</p>
-
-                <figcaption className='absolute  top-28 left-56 m-2 p-1'>Category: {category}</figcaption>
-
-                <p className='absolute top-0 left-56 m-2 p-1'>
-                    {title}</p>
-                <p className='absolute flex right-10 justify-between bottom-1'>
-                    <span className='text-lg font-medium'>{price}</span></p>
+        <article className=' flex flex-col md:flex-row h-60 p-4 justify-between'>
+            <figure className='flex flex-row'>
+                <img className='w-56 h-48 object-cover rounded-lg' src={image} alt={title} />
+                <figcaption className='top-28 left-56 m-2 p-1'>
+                    <p className='m-2'>
+                        {title}
+                    </p>
+                    <p>
+                        Quantity: {quantity}
+                    </p>
+                    <p>
+                        Category: {category}
+                    </p>
+                </figcaption>
 
             </figure>
+
+            <div className='flex flex-col justify-between'>
+                <div className='flex justify-between w-40'>
+                    <button className='m-2'
+                    onClick={() => addToCart({ price, title, category, image, id })} >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+
+                </button>
+                    <button className=' m-2'
+                >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
+                        </svg>
+
+                </button>
+                    <button className='m-2'
+                    onClick={() => removeFromCart({ id })} >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                        </svg>
+
+                    </button>
+
+
+                </div>
+                <p className='flex justify-center mb-8 text-lg font-medium'>
+                    {price}
+                </p>
+            </div>
+
+
+
         </article>
     )
 }
